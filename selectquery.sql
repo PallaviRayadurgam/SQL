09b5ac51d5ac48b44c7737cd17ea57d0730dd64a
PallaviRayadurgam <41808972+PallaviRayadurgam@users.noreply.github.com>
2019-06-21 00:13:04 -0500

    Update selectquery.sql

--- selectquery.sql
+++ selectquery.sql
@@ -7,4 +7,4 @@ select * from CITY where population >=100000 AND countrycode='USA';
 
 --Query the names of all American cities in CITY with populations larger than 120000. The CountryCode for America is USA.
 
-elect name from city where population>120000 and countrycode='USA';
+select name from city where population>120000 and countrycode='USA';
