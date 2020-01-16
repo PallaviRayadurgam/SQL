31e6ba3805ad508ecde922b9d25cfb23a15bcb82
PallaviRayadurgam <41808972+PallaviRayadurgam@users.noreply.github.com>
2020-01-15 21:26:13 -0600

    Update selectquery.sql

--- selectquery.sql
+++ selectquery.sql
@@ -8,3 +8,9 @@ select * from CITY where population >=100000 AND countrycode='USA';
 --Query the names of all American cities in CITY with populations larger than 120000. The CountryCode for America is USA.
 
 select name from city where population>120000 and countrycode='USA';
+
+
+--Query a list of CITY names from STATION with even ID numbers only. You may print the results in any order, but must exclude duplicates from your answer.
+
+
+select DISTINCT CITY from STATION where MOD( ID,2 ) = 0;
